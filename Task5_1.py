c800c3ca0e985dab05d03cc73cb5644c6828b97f
Galina <koroleva2525.gk@gmail.com>
2022-12-26 11:15:48 +0100

    add t

---
+++ Task5_1.py
@@ -0,0 +1,7 @@
+# Реализуйте алгоритм перемешивания списка
+list_len = int(input('введите длину списка: '))
+my_list = []
+for q in range(list_len):
+    my_list.append(input('Введите элемент списка: '))
+    print(my_list)
+    
